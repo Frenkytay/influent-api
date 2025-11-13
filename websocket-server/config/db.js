855c6b95f707b095a8ba32d48faccaae98d00550
Frenkytay <frenky.tay01@gmail.com>
2025-11-13 11:44:33 +0700

    add new

--- websocket-server/config/db.js
+++ websocket-server/config/db.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD || "",
   {
     host: process.env.DB_HOST || "localhost",
-    port: Number(process.env.DB_PORT) || 3306,
+    port: Number(process.env.DB_PORT) || 12945,
     dialect: process.env.DB_DIALECT || "mysql",
     logging: process.env.NODE_ENV === "production" ? false : console.log,
     pool: {
